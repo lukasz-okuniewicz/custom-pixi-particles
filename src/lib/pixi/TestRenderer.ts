739aa73bf5c63f41375bdfed312f50ebf7d00319
Lukasz Okuniewicz <lukasz.okuniewicz@oryxgaming.com>
2022-12-12 22:24:23 +0100

    remove visibility change

--- src/lib/pixi/TestRenderer.ts
+++ src/lib/pixi/TestRenderer.ts
@@ -74,8 +74,6 @@ export default class TestRenderer extends Container {
     if (this.turbulenceEmitter && this.turbulenceEmitter.list) {
       turbulencePool.list = this.turbulenceEmitter.list
     }
-
-    document.addEventListener('visibilitychange', () => this.paused(document.hidden))
   }
 
   pause(isPaused: boolean): void {
