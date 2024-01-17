b273620ea709e5bcaba89ffcb0ea994f07511ee0
Lukasz Okuniewicz <lukasz@okuniewicz.eu>
2024-01-17 17:48:08 +0200

    improvements

--- src/lib/behaviour/ColorBehaviour.ts
+++ src/lib/behaviour/ColorBehaviour.ts
@@ -11,10 +11,6 @@ export default class ColorBehaviour extends Behaviour {
   startVariance = new Color(0, 0, 0, 0)
   endVariance = new Color(0, 0, 0, 0)
   sinus = false
-  _rDiff: number = 0
-  _gDiff: number = 0
-  _bDiff: number = 0
-  _aDiff: number = 0
 
   init = (particle: Particle) => {
     particle.colorStart.copyFrom(this.start)
@@ -30,24 +26,21 @@ export default class ColorBehaviour extends Behaviour {
     particle.colorEnd.alpha += this.varianceFrom(this.endVariance.alpha)
 
     particle.color.copyFrom(particle.colorStart)
-
-    this._rDiff = particle.colorEnd.r - particle.colorStart.r
-    this._gDiff = particle.colorEnd.g - particle.colorStart.g
-    this._bDiff = particle.colorEnd.b - particle.colorStart.b
-    this._aDiff = particle.colorEnd.alpha - particle.colorStart.alpha
   }
 
   apply = (particle: Particle) => {
-    particle.color.copyFrom(particle.colorStart)
+    const { colorStart, lifeProgress, colorEnd } = particle
 
-    particle.color.r += this._rDiff * particle.lifeProgress
-    particle.color.g += this._gDiff * particle.lifeProgress
-    particle.color.b += this._bDiff * particle.lifeProgress
+    particle.color.copyFrom(colorStart)
+
+    particle.color.r += (colorEnd.r - colorStart.r) * lifeProgress
+    particle.color.g += (colorEnd.g - colorStart.g) * lifeProgress
+    particle.color.b += (colorEnd.b - colorStart.b) * lifeProgress
 
     if (!this.sinus) {
-      particle.color.alpha += this._aDiff * particle.lifeProgress
+      particle.color.alpha += (colorEnd.alpha - colorStart.alpha) * lifeProgress
     } else {
-      particle.color.alpha = Math.sin(particle.lifeProgress * 3.1)
+      particle.color.alpha = Math.sin(lifeProgress * 3.1)
     }
   }
 
