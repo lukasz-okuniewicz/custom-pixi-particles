c384e22f3c533266828b24be8d8aeb3f2ca88d27
Lukasz Okuniewicz <lukasz@okuniewicz.eu>
2024-12-20 16:41:08 +0100

    add simplex-noise

--- src/lib/behaviour/NoiseBasedMotionBehaviour.ts
+++ src/lib/behaviour/NoiseBasedMotionBehaviour.ts
@@ -1,3 +1,4 @@
+import { createNoise2D } from 'simplex-noise'
 import BehaviourNames from './BehaviourNames'
 import Particle from '../Particle'
 import { Point } from '../util'
@@ -12,70 +13,16 @@ export default class NoiseBasedMotionBehaviour extends Behaviour {
   noiseSpeed = 0.1
   noiseDirection = new Point(1, 1)
 
-  gradients = new Map<string, Point>()
-  gridSize = 256
+  noise2D: any
 
   constructor() {
     super()
-    this.initializeGradients()
-  }
-
-  initializeGradients() {
-    for (let i = 0; i < this.gridSize; i++) {
-      for (let j = 0; j < this.gridSize; j++) {
-        const angle = Math.random() * Math.PI * 2
-        this.gradients.set(this._gridKey(i, j), new Point(Math.cos(angle), Math.sin(angle)))
-      }
-    }
-  }
-
-  getGradient(ix: number, iy: number): Point {
-    return this.gradients.get(this._gridKey(ix, iy)) || new Point(0, 0)
-  }
-
-  _gridKey(x: number, y: number): string {
-    return `${x % this.gridSize},${y % this.gridSize}`
-  }
-
-  lerp(a: number, b: number, t: number): number {
-    return a + t * (b - a)
-  }
-
-  fade(t: number): number {
-    return t * t * t * (t * (t * 6 - 15) + 10)
-  }
-
-  perlinNoise(x: number, y: number): number {
-    const x0 = Math.floor(x)
-    const x1 = x0 + 1
-    const y0 = Math.floor(y)
-    const y1 = y0 + 1
-
-    const sx = x - x0
-    const sy = y - y0
-
-    const g00 = this.getGradient(x0, y0)
-    const g10 = this.getGradient(x1, y0)
-    const g01 = this.getGradient(x0, y1)
-    const g11 = this.getGradient(x1, y1)
-
-    const dot00 = g00.x * (x - x0) + g00.y * (y - y0)
-    const dot10 = g10.x * (x - x1) + g10.y * (y - y0)
-    const dot01 = g01.x * (x - x0) + g01.y * (y - y1)
-    const dot11 = g11.x * (x - x1) + g11.y * (y - y1)
-
-    const u = this.fade(sx)
-    const v = this.fade(sy)
-
-    const nx0 = this.lerp(dot00, dot10, u)
-    const nx1 = this.lerp(dot01, dot11, u)
-
-    return this.lerp(nx0, nx1, v)
+    this.noise2D = createNoise2D()
   }
 
   init = (particle: Particle) => {
     if (!this.enabled) return
-    particle.noiseOffset = new Point(Math.random() * this.gridSize, Math.random() * this.gridSize)
+    particle.noiseOffset = new Point(Math.random() * 1000, Math.random() * 1000)
   }
 
   apply = (particle: Particle, deltaTime: number) => {
@@ -86,10 +33,9 @@ export default class NoiseBasedMotionBehaviour extends Behaviour {
     noiseOffset.x += this.noiseSpeed * deltaTime * this.noiseDirection.x
     noiseOffset.y += this.noiseSpeed * deltaTime * this.noiseDirection.y
 
-    const noiseX = this.perlinNoise(noiseOffset.x * this.noiseScale, noiseOffset.y * this.noiseScale)
-    const noiseY = this.perlinNoise(noiseOffset.y * this.noiseScale, noiseOffset.x * this.noiseScale)
+    const noiseX = this.noise2D(noiseOffset.x * this.noiseScale, noiseOffset.y * this.noiseScale)
+    const noiseY = this.noise2D(noiseOffset.y * this.noiseScale, noiseOffset.x * this.noiseScale)
 
-    // Adjust movement instead of x, y directly
     particle.movement.x += noiseX * this.noiseIntensity * deltaTime
     particle.movement.y += noiseY * this.noiseIntensity * deltaTime
   }
