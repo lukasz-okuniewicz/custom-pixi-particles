dea1486ef990f95e692dfcd3ca0d8d1bc8d3be35
Lukasz Okuniewicz <lukasz@okuniewicz.eu>
2025-09-17 12:32:46 +0200

    fix deltatime

--- src/lib/pixi/Renderer.ts
+++ src/lib/pixi/Renderer.ts
@@ -163,12 +163,12 @@ export default class Renderer extends Container {
   /**
    * Updates the transform of the ParticleContainer and updates the emitters.
    */
-  _updateTransform(deltaTime: number) {
+  _updateTransform(deltaTime: { deltaTime: number }) {
     if (this._paused) return
 
-    this.emitter?.update(deltaTime)
+    this.emitter?.update(deltaTime.deltaTime)
     if (this.turbulenceEmitter) {
-      this.turbulenceEmitter.update(deltaTime)
+      this.turbulenceEmitter.update(deltaTime.deltaTime)
     }
   }
 
--- src/lib/pixi/TestRenderer.ts
+++ src/lib/pixi/TestRenderer.ts
@@ -157,12 +157,12 @@ export default class TestRenderer extends Container {
   /**
    * Updates the transform of the ParticleContainer and updates the emitters.
    */
-  _updateTransform(deltaTime: number) {
+  _updateTransform(deltaTime: { deltaTime: number }): void {
     if (this._paused) return
 
-    this.emitter?.update(deltaTime)
+    this.emitter?.update(deltaTime.deltaTime)
     if (this.turbulenceEmitter) {
-      this.turbulenceEmitter.update(deltaTime)
+      this.turbulenceEmitter.update(deltaTime.deltaTime)
     }
   }
 
